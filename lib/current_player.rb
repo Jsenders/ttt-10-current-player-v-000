ebcec910a0603da52379058e49e3add709783890
Joseph Senders <jasenders@gmail.com>
2018-05-27 15:09:35 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,2 +1,9 @@
 def turn_count(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+  counter = 1
+  board.each do |position|
+    puts "It is now #{position}'s turn."
+    counter += 1 
+  end
 end
+
+def current_player()
